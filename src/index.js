a56d871952f76c553b8ea9f391eeb2ef66c2c691
jkdcode <jk.didriksen@gmail.com>
2024-01-09 22:30:25 +0100

    js: added real time data via moments and moments.tz + css: slight design adjustments

---
+++ src/index.js
@@ -0,0 +1,57 @@
+function updateTime() {
+  let buenosAiresElement = document.querySelector("#buenos-aires");
+  let buenosAiresDateElement = buenosAiresElement.querySelector(".date");
+  let buenosAiresTimeElement = buenosAiresElement.querySelector(".time");
+  let buenosAiresTime = moment().tz("America/Argentina/Buenos_Aires");
+  let newYorkElement = document.querySelector("#new-york");
+  let newYorkDateElement = newYorkElement.querySelector(".date");
+  let newYorkTimeElement = newYorkElement.querySelector(".time");
+  let newYorkTime = moment().tz("America/New_York");
+  let copenhagenElement = document.querySelector("#copenhagen");
+  let copenhagenDateElement = copenhagenElement.querySelector(".date");
+  let copenhagenTimeElement = copenhagenElement.querySelector(".time");
+  let copenhagenTime = moment().tz("Europe/Copenhagen");
+  let addisAbabaElement = document.querySelector("#addis-ababa");
+  let addisAbabaDateElement = addisAbabaElement.querySelector(".date");
+  let addisAbabaTimeElement = addisAbabaElement.querySelector(".time");
+  let addisAbabaTime = moment().tz("Africa/Addis_Ababa");
+  let tokyoElement = document.querySelector("#tokyo");
+  let tokyoDateElement = tokyoElement.querySelector(".date");
+  let tokyoTimeElement = tokyoElement.querySelector(".time");
+  let tokyoTime = moment().tz("Asia/Tokyo");
+
+  buenosAiresDateElement.innerHTML = moment()
+    .tz("America/Argentina/Buenos_Aires")
+    .format(`MMMM Do, YYYY`);
+  buenosAiresTimeElement.innerHTML = buenosAiresTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+
+  newYorkDateElement.innerHTML = moment()
+    .tz("America/New_York")
+    .format(`MMMM Do, YYYY`);
+  newYorkTimeElement.innerHTML = newYorkTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+
+  copenhagenDateElement.innerHTML = moment()
+    .tz("Europe/Copenhagen")
+    .format(`MMMM Do, YYYY`);
+  copenhagenTimeElement.innerHTML = copenhagenTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+
+  addisAbabaDateElement.innerHTML = moment()
+    .tz("Africa/Addis_Ababa")
+    .format(`MMMM Do, YYYY`);
+  addisAbabaTimeElement.innerHTML = addisAbabaTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+
+  tokyoDateElement.innerHTML = moment()
+    .tz("Asia/Tokyo")
+    .format(`MMMM Do, YYYY`);
+  tokyoTimeElement.innerHTML = tokyoTime.format("h:mm:ss [<small>]A[</small>]");
+}
+updateTime();
+setInterval(updateTime, 1000);
