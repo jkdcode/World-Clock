07ddcaf29434b625649c6ae8ad668c3f8b1e198e
jkdcode <jk.didriksen@gmail.com>
2024-01-10 20:16:35 +0100

    Placeholder text updates + swapped clock font for a monospace one to prevent the element jumping

--- src/index.js
+++ src/index.js
@@ -23,35 +23,27 @@ function updateTime() {
   buenosAiresDateElement.innerHTML = moment()
     .tz("America/Argentina/Buenos_Aires")
     .format(`MMMM Do, YYYY`);
-  buenosAiresTimeElement.innerHTML = buenosAiresTime.format(
-    "h:mm:ss [<small>]A[</small>]"
-  );
+  buenosAiresTimeElement.innerHTML = buenosAiresTime.format("HH:mm:ss");
 
   newYorkDateElement.innerHTML = moment()
     .tz("America/New_York")
     .format(`MMMM Do, YYYY`);
-  newYorkTimeElement.innerHTML = newYorkTime.format(
-    "h:mm:ss [<small>]A[</small>]"
-  );
+  newYorkTimeElement.innerHTML = newYorkTime.format("HH:mm:ss");
 
   edinburghDateElement.innerHTML = moment()
     .tz("Europe/London")
     .format(`MMMM Do, YYYY`);
-  edinburghTimeElement.innerHTML = edinburghTime.format(
-    "h:mm:ss [<small>]A[</small>]"
-  );
+  edinburghTimeElement.innerHTML = edinburghTime.format("HH:mm:ss");
 
   addisAbabaDateElement.innerHTML = moment()
     .tz("Africa/Addis_Ababa")
     .format(`MMMM Do, YYYY`);
-  addisAbabaTimeElement.innerHTML = addisAbabaTime.format(
-    "h:mm:ss [<small>]A[</small>]"
-  );
+  addisAbabaTimeElement.innerHTML = addisAbabaTime.format("HH:mm:ss");
 
   tokyoDateElement.innerHTML = moment()
     .tz("Asia/Tokyo")
     .format(`MMMM Do, YYYY`);
-  tokyoTimeElement.innerHTML = tokyoTime.format("h:mm:ss [<small>]A[</small>]");
+  tokyoTimeElement.innerHTML = tokyoTime.format("HH:mm:ss");
 }
 updateTime();
 setInterval(updateTime, 1000);
