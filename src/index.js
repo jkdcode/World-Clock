d90db1f4f12cb34adaa14bac3af046f2363c126e
jkdcode <jk.didriksen@gmail.com>
2024-01-10 22:41:10 +0100

    Tidying code + added a link to take user back from results page

--- src/index.js
+++ src/index.js
@@ -61,7 +61,8 @@ function updateCities(event) {
         <div class="date">${cityTime.format("MMMM Do, YYYY")}</div>
       </div>
       <div class="time">${cityTime.format("HH:mm:ss")}</div>
-    </div>`;
+    </div>
+    <a href="https://julies-world-clock.netlify.app/" class="back-button">Go back</a>`;
 }
 
 updateTime();
