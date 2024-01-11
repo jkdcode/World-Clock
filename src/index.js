0c5dee8a8f8428de3184f8df42a0c5ab77e42228
jkdcode <jk.didriksen@gmail.com>
2024-01-11 19:03:42 +0100

    Re-arranged some of my js for clarity

--- src/index.js
+++ src/index.js
@@ -1,45 +1,49 @@
 function updateTime() {
+  // Buenos Aires, Argentina
   let buenosAiresElement = document.querySelector("#buenos-aires");
   let buenosAiresDateElement = buenosAiresElement.querySelector(".date");
   let buenosAiresTimeElement = buenosAiresElement.querySelector(".time");
   let buenosAiresTime = moment().tz("America/Argentina/Buenos_Aires");
-  let newYorkElement = document.querySelector("#new-york");
-  let newYorkDateElement = newYorkElement.querySelector(".date");
-  let newYorkTimeElement = newYorkElement.querySelector(".time");
-  let newYorkTime = moment().tz("America/New_York");
-  let edinburghElement = document.querySelector("#edinburgh");
-  let edinburghDateElement = edinburghElement.querySelector(".date");
-  let edinburghTimeElement = edinburghElement.querySelector(".time");
-  let edinburghTime = moment().tz("Europe/London");
-  let addisAbabaElement = document.querySelector("#addis-ababa");
-  let addisAbabaDateElement = addisAbabaElement.querySelector(".date");
-  let addisAbabaTimeElement = addisAbabaElement.querySelector(".time");
-  let addisAbabaTime = moment().tz("Africa/Addis_Ababa");
-  let tokyoElement = document.querySelector("#tokyo");
-  let tokyoDateElement = tokyoElement.querySelector(".date");
-  let tokyoTimeElement = tokyoElement.querySelector(".time");
-  let tokyoTime = moment().tz("Asia/Tokyo");
-
   buenosAiresDateElement.innerHTML = moment()
     .tz("America/Argentina/Buenos_Aires")
     .format(`MMMM Do, YYYY`);
   buenosAiresTimeElement.innerHTML = buenosAiresTime.format("HH:mm:ss");
 
+  // New York, USA
+  let newYorkElement = document.querySelector("#new-york");
+  let newYorkDateElement = newYorkElement.querySelector(".date");
+  let newYorkTimeElement = newYorkElement.querySelector(".time");
+  let newYorkTime = moment().tz("America/New_York");
   newYorkDateElement.innerHTML = moment()
     .tz("America/New_York")
     .format(`MMMM Do, YYYY`);
   newYorkTimeElement.innerHTML = newYorkTime.format("HH:mm:ss");
 
+  // Edinburgh, Scotland
+  let edinburghElement = document.querySelector("#edinburgh");
+  let edinburghDateElement = edinburghElement.querySelector(".date");
+  let edinburghTimeElement = edinburghElement.querySelector(".time");
+  let edinburghTime = moment().tz("Europe/London");
   edinburghDateElement.innerHTML = moment()
     .tz("Europe/London")
     .format(`MMMM Do, YYYY`);
   edinburghTimeElement.innerHTML = edinburghTime.format("HH:mm:ss");
 
+  // Addis Ababa, Ethiopia
+  let addisAbabaElement = document.querySelector("#addis-ababa");
+  let addisAbabaDateElement = addisAbabaElement.querySelector(".date");
+  let addisAbabaTimeElement = addisAbabaElement.querySelector(".time");
+  let addisAbabaTime = moment().tz("Africa/Addis_Ababa");
   addisAbabaDateElement.innerHTML = moment()
     .tz("Africa/Addis_Ababa")
     .format(`MMMM Do, YYYY`);
   addisAbabaTimeElement.innerHTML = addisAbabaTime.format("HH:mm:ss");
 
+  // Tokyo, Japan
+  let tokyoElement = document.querySelector("#tokyo");
+  let tokyoDateElement = tokyoElement.querySelector(".date");
+  let tokyoTimeElement = tokyoElement.querySelector(".time");
+  let tokyoTime = moment().tz("Asia/Tokyo");
   tokyoDateElement.innerHTML = moment()
     .tz("Asia/Tokyo")
     .format(`MMMM Do, YYYY`);
